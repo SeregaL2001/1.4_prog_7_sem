7cedca537cf132fe48504dccf81017aa82132965
SeregaL2001 <92082104+SeregaL2001@users.noreply.github.com>
2022-12-05 16:58:24 +0300

    Update method.h

--- method.h
+++ method.h
@@ -3,7 +3,9 @@
 #include <math.h>
 #include <locale.h> // установка бибилиотеки языков для VScode 
 
+#define TEST 5
 #define EPS 1e-10
+
 
 double solution(double x, int a); // мое подобранное решение под краевые условия 
 
@@ -11,6 +13,6 @@ double g(double x, int a); // действие диф оператора на so
 
 double f(double x, int a, double u_1, double u_2, double u_3, int num); // правая часть для решения Р-К 
 
-void runge_kutta(double *X, int a, double *Res, double y_0, double dy_0, double ddy_0, int N); // метод Р-К см вики 
+void runge_kutta(double *X, int a, double *Res, double u_0, double du_0, double ddu_0, int N); // метод Р-К см вики 
 
 double find_ddu_0(double *X, int a, double u_0, double u_1, double du_0, int N); // поиск краевого условия методом стрельбы 
